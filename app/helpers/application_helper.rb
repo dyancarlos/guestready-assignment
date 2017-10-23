f0de5162ac36f36e3ccb064920ab1a59e0ecc5db
Dyan Carra <dyancarra@gmail.com>
2017-10-23 09:53:36 -0200

    Improve validation error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def validation(object)
+    return unless object
+    if object.errors.any?
+      html = content_tag(:div, content_tag(:h1, t("validations.error")))
+      object.errors.full_messages.each { |msg| html << content_tag(:li, msg) }
+      content_tag(:div, html, class: "validation").html_safe
+    end
+  end
 end

f3b526079b93a6b528a06217b6ca6cc9e219548e
Dyan Carra <dyancarra@gmail.com>
2017-10-24 16:01:40 -0200

    Add profit tests

--- spec/services/profit_service_spec.rb
+++ spec/services/profit_service_spec.rb
@@ -1,5 +1,23 @@
 # coding: utf-8
 require "rails_helper"
 
-describe CurrencyService do
+describe ProfitService do
+  let(:profit) { ProfitService.new(1000, "$700") }
+  let(:not_profit) { ProfitService.new(500, "$900") }
+
+  context "profitable" do
+    it "return the rental difference" do
+      expect(profit.rental_difference).to eq 300
+    end
+
+    it "sould be profitable" do
+      expect(profit.profitable?).to eq "Yes"
+    end
+  end
+
+  context "not profitable" do
+    it "sould be profitable" do
+      expect(not_profit.profitable?).to eq "No"
+    end
+  end
 end
